da6318122415c39a87b1af080d7880f9c24fecce
Ivan Palmegiani <ivan.palmegiani@gmail.com>
2025-01-20 08:39:35 +0100

    improved logging in backup scripts

--- services/piwigo/backup_mariadb.sh
+++ services/piwigo/backup_mariadb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Function to log messages with timestamps
+log_message() {
+    local message="$1"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $message"
+}
+
 # Determine the script's directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -7,36 +13,46 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ -f "$SCRIPT_DIR/.env" ]; then
     source "$SCRIPT_DIR/.env"
 else
-    echo ".env file not found in $SCRIPT_DIR! Exiting."
+    log_message "[MARIADB][ERROR] .env file not found in $SCRIPT_DIR! Exiting."
     exit 1
 fi
 
 # Variables (loaded from .env file or fallback defaults)
 CONTAINER_NAME=mariadb
 BACKUP_DIR=${BACKUP_DIR}
-MARIA_DATABASE=${MARIA_DATABASE}
+MARIADB_DATABASE=${MARIADB_DATABASE}
 MARIADB_USER=${MARIADB_USER}
 MARIADB_PASSWORD=${MARIADB_PASSWORD}
 
 # Get current date and time for backup file
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE=$BACKUP_DIR/${TIMESTAMP}_backup_${MARIA_DATABASE}.sql
+BACKUP_FILE=$BACKUP_DIR/${TIMESTAMP}_backup_${MARIADB_DATABASE}.sql
 
 # Run mysqldump inside the MariaDB container
-docker exec -t $CONTAINER_NAME mysqldump -u $MARIADB_USER -p $MARIADB_PASSWORD $MARIA_DATABASE > $BACKUP_FILE
-echo "[MARIADB] Backup completed: $BACKUP_FILE"
+log_message "[MARIADB] Starting backup..."
+if docker exec -i $CONTAINER_NAME mariadb-dump -u $MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE > $BACKUP_FILE; then
+    if [ -f "$BACKUP_FILE" ]; then
+        log_message "[MARIADB] Backup completed successfully: $BACKUP_FILE"
+    else
+        log_message "[MARIADB][ERROR] Backup failed: Backup file does not exist."
+        exit 1
+    fi
+else
+    log_message "[MARIADB][ERROR] Backup failed: An error occurred during the mysqldump process."
+    exit 1
+fi
 
 # Check and delete backups older than 90 days
-echo "[MARIADB] Checking for backups older than 90 days..."
+log_message "[MARIADB] Checking for backups older than 90 days..."
 OLD_BACKUPS=$(find "$BACKUP_DIR" -type f -mtime +90)
 
 if [ -z "$OLD_BACKUPS" ]; then
-    echo "[MARIADB] No backups older than 90 days found."
+    log_message "[MARIADB] No backups older than 90 days found."
 else
     # Deleting the old backups
     echo "$OLD_BACKUPS" | while read -r FILE; do
-        echo "[MARIADB] Deleting: $FILE"
+        log_message "[MARIADB] Deleting: $FILE"
         rm -f "$FILE"
     done
-    echo "[MARIADB] Old backups deleted successfully."
+    log_message "[MARIADB] Old backups deleted successfully."
 fi
--- services/postgres-cloudbeaver/backup_postgres.sh
+++ services/postgres-cloudbeaver/backup_postgres.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Function to log messages with timestamps
+log_message() {
+    local message="$1"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $message"
+}
+
 # Determine the script's directory
 # SCRIPT_DIR=/home/hs_admin/HomeServer/services/postgres-cloudbeaver
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -8,7 +14,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ -f "$SCRIPT_DIR/.env" ]; then
     source "$SCRIPT_DIR/.env"
 else
-    echo ".env file not found in $SCRIPT_DIR! Exiting."
+    log_message "[POSTGRESQL][ERROR] .env file not found in $SCRIPT_DIR! Exiting."
     exit 1
 fi
 
@@ -24,23 +30,29 @@ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE=$BACKUP_DIR/${TIMESTAMP}_backup_${POSTGRES_DB}.sql
 
 # Run pg_dump inside the PostgreSQL container
-docker exec -t $CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE
-echo "[POSTGRESQL] Backup completed: $BACKUP_FILE"
+log_message "[POSTGRESQL] Starting backup..."
+if docker exec -t $CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE; then
+    if [ -f "$BACKUP_FILE" ]; then
+        log_message "[POSTGRESQL] Backup completed successfully: $BACKUP_FILE"
+    else
+        log_message "[POSTGRESQL][ERROR] Backup failed: Backup file does not exist."
+        exit 1
+    fi
+else
+    log_message "[POSTGRESQL][ERROR] Backup failed: An error occurred during the pg_dump process."
+    exit 1
+fi
 
 # Check and delete backups older than 90 days
-echo "[POSTGRESQL] Checking for backups older than 90 days..."
+log_message "[POSTGRESQL] Checking for backups older than 90 days..."
 OLD_BACKUPS=$(find "$BACKUP_DIR" -type f -mtime +90)
 
 if [ -z "$OLD_BACKUPS" ]; then
-    echo "[POSTGRESQL] No backups older than 90 days found."
+    log_message "[POSTGRESQL] No backups older than 90 days found."
 else
-    # echo "The following backups are older than 90 days and will be deleted:"
-    # echo "$OLD_BACKUPS"
-
-    # Deleting the old backups
     echo "$OLD_BACKUPS" | while read -r FILE; do
-        echo "[POSTGRESQL] Deleting: $FILE"
+        log_message "[POSTGRESQL] Deleting: $FILE"
         rm -f "$FILE"
     done
-    echo "[POSTGRESQL] Old backups deleted successfully."
+    log_message "[POSTGRESQL] Old backups deleted successfully."
 fi
